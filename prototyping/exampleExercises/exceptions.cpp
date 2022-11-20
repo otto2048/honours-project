0932255a82975dfbf8371702958bcd87716ea894
otto2048 <91140129+otto2048@users.noreply.github.com>
2022-11-20 17:11:48 +0000

    Start creating logical error exercises

---
+++ prototyping/exampleExercises/exceptions.cpp
@@ -0,0 +1,12 @@
+double divisor(int a, int b)
+{
+    return a/b;
+}
+
+int main()
+{
+    int a = 9;
+    int b = 0;
+
+    divisor(a, b);
+}
---
+++ prototyping/exampleExercises/logicErrors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int c = 0;
+
+    c = a * b;
+
+    cout << "A divided by B is: " << c << endl;
+
+    
+}
