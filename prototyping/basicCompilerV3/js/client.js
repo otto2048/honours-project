923cf93a92f7c510171a49bebe5fd20c24470a1c
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-02 14:21:59 +0000

    Copy basic compiler into V3 folder

---
+++ prototyping/basicCompilerV3/js/client.js
@@ -0,0 +1,65 @@
+// Socket setup based on tutorial: https://javascript.info/websocket
+
+window.onload = preparePage();
+
+let socket = new WebSocket("ws://192.168.17.50:8080");
+
+//set up socket
+socket.onopen = function(e) {
+    console.log("Connection established");
+};
+
+socket.onmessage = function(event) {
+    //get active terminal
+    var term = $.terminal.active();
+
+    //output received message into terminal
+    term.echo(event.data);
+};
+
+socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log("Connection closed cleanly, code=${event.code} reason=${event.reason}");
+    } else {
+        console.log("Connection died");
+    }
+};
+
+socket.onerror = function(error) {
+    console.log("[error]");
+};
+
+function preparePage()
+{
+    //add event listener to play button
+    document.getElementById("play-btn").addEventListener("click", startProgram);
+
+    //set up jquery terminal
+    $('#code-output').terminal(function(command)
+    {
+        if (command !== '')
+        {
+            sendInput(command);
+        }
+    }, {
+        height: 500
+    });
+}
+
+//tell socket that we want to start playing the program
+function startProgram()
+{
+    var obj = new Object();
+    obj.operation = "PLAY";
+    obj.value = true;
+    socket.send(JSON.stringify(obj));
+}
+
+//tell socket that we want to send some input to the program
+function sendInput(input)
+{
+    var obj = new Object();
+    obj.operation = "INPUT";
+    obj.value = input;
+    socket.send(JSON.stringify(obj));
+}
---
+++ prototyping/basicCompilerV3/js/server.js
@@ -0,0 +1,64 @@
+// Socket setup based on tutorial: https://javascript.info/websocket
+//include required modules
+const http = require('http');
+const ws = require('ws');
+const spawn = require('child_process').spawn;
+
+const wss = new ws.Server({noServer: true});
+
+//variable to hold child process that runs program
+var progProcess;
+
+//create server
+function accept(req, res) {
+    // all incoming requests must be websockets
+    if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
+        res.end();
+        return; 
+    }
+
+    // can be Connection: keep-alive, Upgrade
+    if (!req.headers.connection.match(/\bupgrade\b/i)) {
+        res.end();
+        return;
+    }
+
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
+}
+
+http.createServer(accept).listen(8080);
+
+//handle connections
+function onConnect(ws) {
+    ws.on('message', function (message) {
+        const clientMsg = JSON.parse(message);
+
+        //check which operation client has requested
+        if (clientMsg.operation == "PLAY")
+        {
+            //use child process to start program
+            progProcess = spawn('./executable');
+
+            progProcess.stdout.on('data', function (data) {
+                console.log('stdout: ' + data.toString());
+                ws.send(data.toString());
+            });
+
+            progProcess.stderr.on('data', function (data) {
+                console.log('stderr: ' + data.toString());
+				ws.send(data.toString());
+            });
+
+            progProcess.on('exit', function (code) {
+				var data = 'Program exited with code ' + code.toString();
+                console.log();
+				ws.send(data);
+            });
+        }
+        else if (clientMsg.operation == "INPUT")
+        {
+            //send input to child process
+            progProcess.stdin.write(clientMsg.value + "\n");
+        }
+    });
+}
