3c541813112e53f5926f5d8980b268d1e74bfa25
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-06 17:11:49 +0000

    Update userDashboard.php

--- implementation/webapp/view/adminArea/users/userDashboard.php
+++ implementation/webapp/view/adminArea/users/userDashboard.php
@@ -52,118 +52,124 @@
                 }
             ?>
 
-            <?php
 
-                //get first page of users
+            <div class="row">
+                <div class="col border-end">
+                    <?php
 
-                $pageSize = 10;
-                $pageLimit = 0;
+                        //get first page of users
 
-                $userModel = new UserModel();
+                        $pageSize = 10;
+                        $pageLimit = 0;
 
-                $jsonUserData = $userModel->getUsers(1, $pageSize, $pageLimit);
+                        $userModel = new UserModel();
 
-                $userData = json_decode($jsonUserData, JSON_INVALID_UTF8_SUBSTITUTE);
+                        $jsonUserData = $userModel->getUsers(1, $pageSize, $pageLimit);
 
-                if (!isset($userData["isempty"]))
-                {
-            ?>
+                        $userData = json_decode($jsonUserData, JSON_INVALID_UTF8_SUBSTITUTE);
 
-                <!-- view users table -->
-                <p class="pb-1 pt-3 mb-0">Click on column headings to sort Users by this column</p>
-                <div class="table-responsive">
-                    <table class="table tablesort tablesearch-table paginateTable" id="userInfoTable">
-                        <thead>
-                            <tr>
-                                <th scope="col" data-tablesort-type="int">ID</th>
-                                <th scope="col" data-tablesort-type="string" class="d-none d-sm-none d-md-table-cell">Username</th>
-                                <th scope="col" data-tablesort-type="string" class="d-none d-sm-none d-md-table-cell">Container port</th>
-                                <th scope="col" data-tablesort-type="string">User Group</th>
-                                <th scope="col">Link to User page</th>
-                            </tr>
-                        </thead>
-                        <tbody class="paginateTableBody" id="userInfoTableBody">
-                            <?php
+                        if (!isset($userData["isempty"]))
+                        {
+                    ?>
 
-                                //display current permission
-                                $permission = new PermissionLevels();
+                            <!-- view users table -->
+                            <p class="pb-1 pt-3 mb-0">Click on column headings to sort Users by this column</p>
+                            <div class="table-responsive">
+                                <table class="table tablesort tablesearch-table paginateTable" id="userInfoTable">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" data-tablesort-type="int">ID</th>
+                                            <th scope="col" data-tablesort-type="string" class="d-none d-sm-none d-md-table-cell">Username</th>
+                                            <th scope="col" data-tablesort-type="string" class="d-none d-sm-none d-md-table-cell">Container port</th>
+                                            <th scope="col" data-tablesort-type="string">User Group</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="paginateTableBody" id="userInfoTableBody">
+                                        <?php
 
-                                //display user data
-                                foreach ($userData as $row)
-                                {
-                                    echo '<tr>';
-                                    echo '<td>'.$row["userId"].'</td>';
+                                            //display current permission
+                                            $permission = new PermissionLevels();
 
-                                    echo '<td><u><a href="user.php?id='.$row["userId"].'" class="moreInfoLink">'.$row["username"].'</a></u></td>';
-                                    echo '<td class="d-none d-sm-none d-md-table-cell">'.$row["containerPort"].'</td>';
+                                            //display user data
+                                            foreach ($userData as $row)
+                                            {
+                                                echo '<tr>';
+                                                echo '<td>'.$row["userId"].'</td>';
 
-                                    echo '<td>'.$permission->getPermissionLevel($row["permissionLevel"]).'</td>';
+                                                echo '<td><u><a href="user.php?id='.$row["userId"].'" class="moreInfoLink">'.$row["username"].'</a></u></td>';
+                                                echo '<td class="d-none d-sm-none d-md-table-cell">'.$row["containerPort"].'</td>';
 
-                                    echo '<td><a href="user.php?id='.$row["userId"].'" class="btn theme-darker text-light" role="button">More info...</a></td>';
-                                    echo '</tr>';
-                                }
-                            ?>
-                        </tbody>
-                    </table>
+                                                echo '<td>'.$permission->getPermissionLevel($row["permissionLevel"]).'</td>';
+                                                echo '</tr>';
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
 
-                    
-                </div>
-
-
-                <button class="btn theme-darker text-light" id="previousPageBtn">Previous page</button>
-                <button class="btn theme-darker text-light float-end" id="nextPageBtn">Next page</button>
-
-                <p class="text-center">Page: <span id="pageNum">1</span>/<span id="totalPages"><?php echo $pageLimit ?></span></p>
-
-                <p>Page Size: <span id="pageSize"><?php echo $pageSize ?></span></p>
-            <?php
-
-                }
-                else
-                {
-                    echo "Failed to load user data";
-                }                  
-            ?>
+                                
+                            </div>
 
 
-        <!-- create new user -->
-            <hr>
-            <h2>Create a new user</h2>
-            <form role="form" method="POST" action="../../../controller/actionScripts/createUser.php">
-                <div class="form-group">
-                    <label for="username">Username:</label>
-                    <input type="text" class="form-control" name="username" required id="username">
-                </div>
-                <div class="form-group">
-                    <label for="containerPort">Container Port:</label>
-                    <input type="text" class="form-control" name="containerPort" id="containerPort">
-                </div>
-                <div class="form-group">
-                    <label for="password">Password:</label>
-                    <input type="password" class="form-control" name="password" required id="password">
-                </div>
-                <div class="form-group pt-1">
-                    <label for="permissionLevel">User group:</label>
-                    <select name="permissionLevel" id="permissionLevel">
+                            <button class="btn theme-darker text-light" id="previousPageBtn">Previous page</button>
+                            <button class="btn theme-darker text-light float-end" id="nextPageBtn">Next page</button>
+
+                            <p class="text-center">Page: <span id="pageNum">1</span>/<span id="totalPages"><?php echo $pageLimit ?></span></p>
+
+                            <p>Page Size: <span id="pageSize"><?php echo $pageSize ?></span></p>
                         <?php
-                            $permissionReflection = new \ReflectionClass("PermissionLevels");
-                            $values = $permissionReflection->getConstants();
 
-                            foreach ($values as $value)
-                            {
-                                $optionString = '<option value = "';
-                                $optionString .= $value.'"';
-                                $optionString .= ">".$permission->getPermissionLevel($value)."</option>";
-
-                                echo $optionString;
                             }
+                            else
+                            {
+                                echo "Failed to load user data";
+                            }                  
                         ?>
-                    </select>
+
                 </div>
-                <button class="btn btn-dark float-end mt-2" type="submit">Submit</button>
-            </form>
-        
-        </div>
+                <div class="col">
+                    <!-- create new user -->
+                    <h2>Create a new user</h2>
+                    <form role="form" method="POST" action="../../../controller/actionScripts/createUser.php">
+                        <div class="form-group">
+                            <label for="username">Username:</label>
+                            <input type="text" class="form-control" name="username" required id="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="containerPort">Container Port:</label>
+                            <input type="text" class="form-control" name="containerPort" id="containerPort">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="password" class="form-control" name="password" required id="password">
+                        </div>
+                        <div class="form-group pt-1">
+                            <label for="permissionLevel">User group:</label>
+                            <select name="permissionLevel" id="permissionLevel">
+                                <?php
+                                    $permissionReflection = new \ReflectionClass("PermissionLevels");
+                                    $values = $permissionReflection->getConstants();
+
+                                    foreach ($values as $value)
+                                    {
+                                        $optionString = '<option value = "';
+                                        $optionString .= $value.'"';
+                                        $optionString .= ">".$permission->getPermissionLevel($value)."</option>";
+
+                                        echo $optionString;
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <button class="btn btn-dark float-end mt-2" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+            
+
+
+
+
+    </div>
         
         <!-- Auto tables plugin -->
         <script src="../../js/auto-sorter-filter/auto-tables.js"></script>
