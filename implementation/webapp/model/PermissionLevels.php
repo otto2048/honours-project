e49a4ebfb6f05b5f42acb1e3bd1f01064d9172d4
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-06 11:42:06 +0000

    Output the first page of users on the user dashboard

--- implementation/webapp/model/PermissionLevels.php
+++ implementation/webapp/model/PermissionLevels.php
@@ -6,6 +6,25 @@
         const GUEST = 2;
         const EXPERIMENT = 1;
         const CONTROL = 0;
+
+        //get string value associated with user permission
+        public function getPermissionLevel($state)
+        {
+            switch($state)
+            {
+                case PermissionLevels::ADMIN:
+                    return "Admin Group";
+                case PermissionLevels::GUEST:
+                    return "Guest Group";
+                case PermissionLevels::EXPERIMENT:
+                    return "Experimental Group";
+                case PermissionLevels::CONTROL:
+                    return "Control Group";
+                default:
+                    return "Error finding status";
+            }
+        }
     }
 
+    
 ?>
--- implementation/webapp/view/adminArea/users/userDashboard.php
+++ implementation/webapp/view/adminArea/users/userDashboard.php
@@ -32,14 +32,86 @@
 
             getHeader();
 
+        ?>
+            <div class="container-fluid">
+
+        <?php
+
+            //get first page of users
+            require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/model/UserModel.php");
+
+            $pageSize = 10;
+
+            $userModel = new UserModel();
+
+            $jsonUserData = $userModel->getUsers(1, $pageSize);
+
+            if ($jsonUserData != null)
+            {
+                $userData = json_decode($jsonUserData, JSON_INVALID_UTF8_SUBSTITUTE);
+
          ?>
 
-         <!-- view users table -->
+                <!-- view users table -->
+                <p class="pb-1 pt-3 mb-0">Click on column headings to sort Users by this column</p>
+                <div class="table-responsive">
+                    <table class="table tablesort tablesearch-table" id="userInfoTable">
+                        <thead>
+                            <tr>
+                                <th scope="col" data-tablesort-type="int">ID</th>
+                                <th scope="col" data-tablesort-type="string" class="d-none d-sm-none d-md-table-cell">Username</th>
+                                <th scope="col" data-tablesort-type="string" class="d-none d-sm-none d-md-table-cell">Container port</th>
+                                <th scope="col" data-tablesort-type="string">User Group</th>
+                                <th scope="col">Link to User page</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+
+                                //display current permission
+                                $permission = new PermissionLevels();
+
+                                //display user data
+                                foreach ($userData as $row)
+                                {
+                                    echo '<tr>';
+                                    echo '<td>'.$row["userId"].'</td>';
+
+                                    echo '<td><u><a href="user.php?id='.$row["id"].'" class="moreInfoLink">'.$row["username"].'</a></u></td>';
+                                    echo '<td class="d-none d-sm-none d-md-table-cell">'.$row["containerPort"].'</td>';
+
+                                    echo '<td>'.$permission->getPermissionLevel($row["permissionLevel"]).'</td>';
+
+                                    echo '<td><a href="user.php?id='.$row["id"].'" class="btn theme-darker text-light" role="button">More info...</a></td>';
+                                    echo '</tr>';
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+
+                    <button class="btn theme-darker text-light">Previous page</button>
+                    <button class="btn theme-darker text-light float-end">Next page</button>
+                </div>
+
+        <?php
+
+            }
+            else
+            {
+                echo "Failed to load user data";
+            }                  
+        ?>
+
 
          <!-- create new user -->
 
          <!-- update user -->
 
          <!-- delete user -->
+
+         <!-- Auto tables plugin -->
+        
+        </div>
+        <script src="../../js/auto-sorter-filter/auto-tables.js"></script>
     </body>
 </html>
