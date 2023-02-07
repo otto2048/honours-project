6baf93c5fdf77e944166cd0259cc45167f30a9c5
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-07 11:31:05 +0000

    Create user sign up page with psuedo code

--- implementation/webapp/controller/logout.php
+++ implementation/webapp/controller/logout.php
@@ -5,4 +5,6 @@
     session_unset();
 
     session_destroy();
+
+    // TODO: if this user is a guest, delete all of their data
 ?>
---
+++ implementation/webapp/view/userArea/signUp.php
@@ -0,0 +1,10 @@
+<?php
+
+    // sign up to the study
+
+    // users give consent to take part in the study and have their data saved
+    //      if users don't want to take part, they are given a guest account
+    //      else, users are given a random username and set their own password
+    //            users are assigned into the control or experimental group
+
+?>
