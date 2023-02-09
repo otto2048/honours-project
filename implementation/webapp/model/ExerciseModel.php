a0ef7aeb9550c5dad2934f3189578965c479f111
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-09 16:14:46 +0000

    Display a users assigned exercises

--- implementation/webapp/model/ExerciseModel.php
+++ implementation/webapp/model/ExerciseModel.php
@@ -54,7 +54,7 @@
             $WHERE_variables = new \stdClass();
             $WHERE_variables -> permission = $permission;
 
-            $this->sqlStmt = 'SELECT codeId, title, visible
+            $this->sqlStmt = 'SELECT codeId, title, visible, description
                 FROM honours_code_exercise WHERE availability <= ?';
 
             $paramTypes = "i";
