30ed488f8fea51b8450d31c575c007642d16ba98
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-10 15:13:07 +0000

    Add some example exercises

---
+++ implementation/webapp/view/exerciseFiles/logicalError/logicalError.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+//find the largest of 2 numbers
+int largest(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    
+    return b;
+}
+
+//find the largest of 3 numbers
+int largest3(int a, int b, int c)
+{
+    // NEED TO CORRECT THIS LINE
+    //if (a >= b && a >= c)
+    if (a > b && a > c)
+    {
+        return a;
+    }
+    // NEED TO CORRECT THIS LINE
+    //else if (b >= a && b >= c)
+    else if (b > a && b > c)
+    {
+        return b;
+    }
+    else
+    {
+        return c;
+    }
+}
+
+//find the largest of 6 numbers
+int largest6(int a, int b, int c, int d, int e, int f)
+{
+    return largest(largest3(a, b, c), largest3(d, e, f));
+}
+
+int main()
+{
+    int u = 5;
+    int v = 5;
+    int w = 1;
+    int x = 2;
+    int y = 4;
+    int z = 1;
+
+    cout << largest6(u, v, w, x, y, z) << endl;
+}
