a48429d23cb6f58bafe86a3d840034dda48da5b4
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-10 17:38:58 +0000

    Change input addition

--- implementation/webapp/controller/controllers/ExerciseAnswerController.php
+++ implementation/webapp/controller/controllers/ExerciseAnswerController.php
@@ -21,10 +21,30 @@
         {
             //exercise input into json object
             $data = new \stdClass();
-            $data -> codeId_fk = $_POST["codeId"];
-            $data -> input = $_POST["input"];
-            $data -> inputType = $_POST["inputType"];
-            $data -> output = $_POST["output"];
+
+            $data -> answer = new \stdClass();
+            $data -> answer -> codeId_fk = $_POST["codeId"];
+            $data -> answer -> output = $_POST["output"];
+
+            $data -> inputs = new \stdClass();
+
+            foreach ($_POST as $key => $post)
+            {
+                if (strpos($key, "input") !== false && strpos($key, "inputType") === false)
+                {
+                    $data -> inputs -> $key = new \stdClass();
+                    $data -> inputs -> $key -> value = $post;
+                }
+            }
+
+            foreach ($_POST as $key => $post)
+            {
+                if (strpos($key, "Type") !== false)
+                {
+                    $input = "input".substr($key, -1);
+                    $data -> inputs -> $input -> type = $post;
+                }
+            }
 
             $jsonData = json_encode($data, JSON_INVALID_UTF8_SUBSTITUTE);
 
@@ -33,14 +53,14 @@
             $successMessage[0]["content"] = "Successfully created exercise answer";
 
             $this->successPathVariables["message"] = json_encode($successMessage);
-            $this->successPathVariables["id"] = $this->validationObj->cleanInput($data->codeId_fk);
+            $this->successPathVariables["id"] = $this->validationObj->cleanInput($data->answer->codeId_fk);
 
             //prepare error message
             $failureMessage[0]["success"] = false;
             $failureMessage[0]["content"] = "Failed to create exercise answer. Try again?";
 
             $this->failurePathVariables["message"] = json_encode($failureMessage);
-            $this->failurePathVariables["id"] = $this->validationObj->cleanInput($data->codeId_fk);
+            $this->failurePathVariables["id"] = $this->validationObj->cleanInput($data->answer->codeId_fk);
 
             //set success and failure paths
             $this->successPath = "/honours/webapp/view/adminArea/exercises/exercise.php";
--- implementation/webapp/model/ExerciseAnswerModel.php
+++ implementation/webapp/model/ExerciseAnswerModel.php
@@ -36,11 +36,65 @@
         //add an exercise answer
         public function createData($jsonData)
         {
-            $this->sqlStmt = 'INSERT INTO honours_code_answer (codeId_fk, input, inputType, output) VALUES (?, ?, ?, ?)';
-            
-            $paramTypes = "isis";
+            //sort data into answer and inputs
+            $data = json_decode($jsonData, JSON_INVALID_UTF8_SUBSTITUTE);
 
-            return parent::create($jsonData, $paramTypes);
+            $answer = $data["answer"];
+            $inputs = $data["inputs"];
+
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+            //start transaction
+            mysqli_begin_transaction($this->conn->getConnection());
+
+            try
+            {
+                //create answer
+                $this->sqlStmt = 'INSERT INTO honours_code_answer (codeId_fk, output) VALUES (?, ?)';
+
+                $paramTypes = "is";
+
+                $answerId = -1;
+
+                parent::create(json_encode($answer, JSON_INVALID_UTF8_SUBSTITUTE), $paramTypes, $null, $answerId);
+
+                //create the inputs for the answer
+                $this->sqlStmt = 'INSERT INTO honours_values (value, type, codeAnswer_fk) VALUES (?, ?, ?)';
+
+                foreach ($inputs as $input)
+                {
+                    $variables = new \stdClass();
+                    $variables->value = $input["value"];
+                    $variables->type = $input["type"];
+                    $variables->codeAnswer_fk = $answerId;
+
+                    $paramTypes = "sii";
+
+                    parent::create(json_encode($variables, JSON_INVALID_UTF8_SUBSTITUTE), $paramTypes);
+                }
+
+                mysqli_commit($this->conn->getConnection());
+
+                return true;
+            }
+            catch (mysqli_sql_exception $exception)
+            {
+                echo "here";
+                echo "<pre>";
+                var_dump($exception);
+                echo "</pre>";
+                mysqli_rollback($this->conn->getConnection());
+
+                return false;
+            }
+            catch (Exception $e)
+            {
+                echo "here 2";
+
+                return false;
+            }
+
+            return false;
         }
     }
 
