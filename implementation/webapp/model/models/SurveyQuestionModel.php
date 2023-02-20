7240b593fe752584cb485cbc5591047d8edc41b5
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-20 14:05:32 +0000

    Create model class skeletons

---
+++ implementation/webapp/model/models/SurveyQuestionModel.php
@@ -0,0 +1,19 @@
+<?php
+    //model class to interface with the honours_survey_question table (SUS survey questions)
+
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/model/Model.php");
+
+    class SurveyQuestionModel extends Model
+    {
+        //get survey question
+
+        //get all survey questions
+
+        //delete survey question
+
+        //create survey question
+
+        //update survey question
+    }
+
+?>
---
+++ implementation/webapp/model/models/UserSurveyModel.php
@@ -0,0 +1,12 @@
+<?php
+    //model class to interface with honours_user_survey table (users answers to survey questions)
+
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/model/Model.php");
+
+    class UserSurveyModel extends Model
+    {
+        //create record
+
+        //get all the records for a user
+    }
+?>
