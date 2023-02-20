ddab5be865625efe3db029e0ba588bfbe244e0d9
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-20 14:12:55 +0000

    Update SurveyQuestionModel.php

--- implementation/webapp/model/models/SurveyQuestionModel.php
+++ implementation/webapp/model/models/SurveyQuestionModel.php
@@ -6,14 +6,57 @@
     class SurveyQuestionModel extends Model
     {
         //get survey question
+        public function getSurveyQuestion($questionId)
+        {
+            $this->sqlStmt = 'SELECT *
+            FROM honours_survey_question
+            WHERE honours_survey_question.questionId=?';
+
+            $WHERE_variables = new \stdClass();
+            $WHERE_variables -> questionId = $questionId;
+
+            $paramTypes = "i";
+
+            return parent::retrieve(json_encode($WHERE_variables, JSON_INVALID_UTF8_SUBSTITUTE), $paramTypes);
+        }
 
         //get all survey questions
+        public function getAllSurveyQuestions()
+        {
+            $this->sqlStmt = 'SELECT * FROM honours_survey_question';
+
+            return parent::retrieve();
+        }
 
         //delete survey question
+        public function deleteData($jsonData)
+        {
+            $this->sqlStmt = 'DELETE FROM honours_survey_question WHERE questionId = ?';
+            
+            $paramTypes = "i";
+
+            return parent::delete($jsonData, $paramTypes);
+        }
 
         //create survey question
+        public function createData($jsonData)
+        {
+            $this->sqlStmt = 'INSERT INTO honours_survey_question (questionId, contents) VALUES (?, ?)';
+
+            $paramTypes = "is";
+
+            return parent::create($jsonData, $paramTypes);
+        }
 
         //update survey question
+        public function updateData($jsonData)
+        {
+            $this->sqlStmt = 'UPDATE honours_survey_question SET contents = ? WHERE questionId = ?';
+
+            $paramTypes = "si";
+
+            return parent::update($jsonData, $paramTypes);
+        }
     }
 
 ?>
