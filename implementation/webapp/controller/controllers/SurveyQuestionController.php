bbc6df7db68b87e44d2d42fb348cdd80d7887cbe
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-02-20 15:04:28 +0000

    Allow user to update a survey question

--- implementation/webapp/controller/controllers/SurveyQuestionController.php
+++ implementation/webapp/controller/controllers/SurveyQuestionController.php
@@ -61,6 +61,36 @@
 
             return parent::delete($jsonData);
         }
+
+        public function updateSurveyQuestion()
+        {
+            //user input into json object
+            $data = new \stdClass();
+            $data -> contents = $_POST['contents'];
+            $data -> questionId = $_POST['questionId'];
+
+            $jsonData = json_encode($data, JSON_INVALID_UTF8_SUBSTITUTE);
+
+            //prepare success message
+            $successMessage[0]["success"] = true;
+            $successMessage[0]["content"] = "Successfully updated survey question";
+
+            $this->successPathVariables["message"] = json_encode($successMessage);
+            $this->successPathVariables["id"] = $this->validationObj->cleanInput($data->questionId);
+
+            //prepare error message
+            $failureMessage[0]["success"] = false;
+            $failureMessage[0]["content"] = "Failed to update survey question. Try again?";
+
+            $this->failurePathVariables["message"] = json_encode($failureMessage);
+            $this->failurePathVariables["id"] = $this->validationObj->cleanInput($data->questionId);
+
+            //set success and failure paths
+            $this->successPath = "/honours/webapp/view/adminArea/survey/survey.php";
+            $this->failurePath = "/honours/webapp/view/adminArea/survey/updateSurvey.php";
+
+            return parent::update($jsonData);
+        }
     }
 
 ?>
---
+++ implementation/webapp/view/adminArea/survey/updateSurvey.php
@@ -0,0 +1,117 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/controller/Session.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/model/PermissionLevels.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/model/models/SurveyQuestionModel.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/controller/Validation.php");
+
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/view/printErrorMessages.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/view/navigation.php");
+
+    //check if the user is allowed to be here
+    if (!isset($_SESSION["permissionLevel"]))
+    {
+        echo '<script type="text/javascript">window.open("/honours/webapp/view/index.php", name="_self")</script>';
+    }
+
+    if ($_SESSION["permissionLevel"] < PermissionLevels::ADMIN)
+    {
+        echo '<script type="text/javascript">window.open("/honours/webapp/view/index.php", name="_self")</script>';
+    }
+?>
+
+<!doctype html>
+
+<html lang="en" data-bs-theme="dark">
+    <head>
+        <title>Debugging Training Tool - Update Survey Question</title>
+        <?php include "../../head.php"; ?>
+    </head>
+    <body>
+        <?php
+            getNavigation();
+        ?>
+        
+        <div class="container p-3">
+            
+            <?php
+
+                $validation = new Validation();
+
+                //sanitize input
+                $input = $validation->cleanInput($_GET["id"]);
+
+                //validate input
+                if ($validation->validateInt($input))
+                {
+                    //get survey question
+                    $surveyQuestionModel = new SurveyQuestionModel();
+
+                    $jsonQuestionData = $surveyQuestionModel->getSurveyQuestion($input);
+
+                    if ($jsonQuestionData)
+                    {
+                        $questionData = json_decode($jsonQuestionData, JSON_INVALID_UTF8_SUBSTITUTE);
+
+                        if (!isset($questionData["isempty"]))
+                        {
+                            //display survey question details 
+                    ?>
+                            <h1>Update Survey Question - <?php echo $questionData[0]["questionId"]?></h1>
+
+                            <?php
+                                //check for errors on this page
+                                if (isset($_GET["message"]))
+                                {
+                                    $message = $_GET["message"];
+                                
+                                    printErrorMessage($message);
+                                }
+                            ?>
+
+                            <!-- update survey question -->
+                            <form role="form" method="POST" action="../../../controller/actionScripts/updateSurveyQuestion.php">
+                                <input type="text" name="questionId" value=<?php echo $questionData[0]["questionId"] ?> required hidden readonly>
+                                <div class="form-group">
+                                    <label for="contents">Contents:</label>
+                                    <input type="text" class="form-control" name="contents" required id="contents" value="<?php echo $questionData[0]["contents"] ?>">
+                                </div>
+                                <button class="btn btn-primary float-end mt-2" type="submit">Submit</button>
+                            </form>
+                            
+                        </div>
+
+                    <?php
+
+                        }
+                        else
+                        {
+                    ?>
+                            <h1>Update Survey Question</h1>
+                    <?php
+                            echo "Failed to load survey question data";
+                        }
+                    }
+                    else
+                    {
+                        ?>
+                            <h1>Update Survey Question</h1>
+                    <?php
+                            echo "Failed to load survey question data";
+                    }
+                }
+                else
+                {
+                ?>
+                        <h1>Update Survey Question</h1>
+                <?php
+                    echo "Failed to load survey question data";
+                }             
+            ?>
+
+        
+        </div>
+
+        <script src="../../js/setTheme.js"></script>
+
+    </body>
+</html>
