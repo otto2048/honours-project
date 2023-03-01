699a422c829045548c9e287049cb03062a7b91eb
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-03-01 16:05:38 +0000

    Add response class to host server

---
+++ implementation/debugger/response.js
@@ -0,0 +1,10 @@
+class Response {
+    constructor(sender_) {
+        this.sender = sender_;
+        this.event = null;
+        this.value = null;
+        this.message = null;
+    }
+}
+
+module.exports = Response;
