c5788e63dbd509037b67088e9d7be4eefebfa032
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-03-06 16:52:36 +0000

    Create pretty printer for std::string

---
+++ implementation/debugger/debuggerApp/app/gdbPrettyPrinter.py
@@ -0,0 +1,19 @@
+import gdb
+
+#https://sourceware.org/gdb/onlinedocs/gdb/Writing-a-Pretty_002dPrinter.html
+class StringPrettyPrinter(object):
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        return self.val['_M_dataplus']['_M_p']
+    
+def build_pretty_printer():
+    pp = gdb.printing.RegexpCollectionPrettyPrinter(
+        "my_library")
+    pp.add_printer('std::__cxx11::basic_string<char,.*>', '^std::__cxx11::basic_string<char,.*>$', StringPrettyPrinter)
+    return pp
+
+gdb.printing.register_pretty_printer(
+    gdb.current_objfile(),
+    build_pretty_printer())
