7485bf33fbd78e3171ed2d129bc877936991b580
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-03-20 11:49:30 +0000

    Create some unit tests

---
+++ implementation/webapp/view/exerciseFiles/carsOnSale/unit_test.cpp
@@ -0,0 +1,32 @@
+#include "Exercise.h"
+#include "gtest/gtest.h"
+
+namespace {
+	TEST(carsOnSale, test1)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(8, garage.carsOnSale(2, 5, true));
+	}
+
+    TEST(carsOnSale, test2)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(4, garage.carsOnSale(2, 5, false));
+	}
+
+    TEST(carsOnSale, test3)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.carsOnSale(5, 5, true));
+	}
+
+    TEST(carsOnSale, test4)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(1, garage.carsOnSale(5, 5, false));
+	}
+}
--- implementation/webapp/view/exerciseFiles/highest/unit_test.cpp
+++ implementation/webapp/view/exerciseFiles/highest/unit_test.cpp
@@ -8,4 +8,46 @@ namespace {
 
 		EXPECT_EQ(5, garage.largestPrice(5, 5, 1, 2, 4, 1).getId());
 	}
+
+	TEST(largestPrice, test2)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.largestPrice(1, 5, 5, 2, 4, 1).getId());
+	}
+
+	TEST(largestPrice, test3)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.largestPrice(5, 1, 5, 2, 4, 1).getId());
+	}
+
+	TEST(largestPrice, test4)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.largestPrice(5, 5, 5, 2, 4, 1).getId());
+	}
+
+	TEST(largestPrice, test5)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.largestPrice(5, 2, 1, 2, 4, 1).getId());
+	}
+
+	TEST(largestPrice, test6)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.largestPrice(2, 5, 1, 2, 4, 1).getId());
+	}
+
+	TEST(largestPrice, test7)
+	{
+        Exercise garage;
+
+		EXPECT_EQ(5, garage.largestPrice(1, 2, 5, 2, 4, 1).getId());
+	}
 }
