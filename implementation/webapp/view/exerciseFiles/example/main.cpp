6dc1c9cc6ebf482b00a8efdb74d1075f6e6ae3a1
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-04-13 13:51:52 +0100

    Create example exercise

---
+++ implementation/webapp/view/exerciseFiles/example/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+// return the largest integer out of two inputs
+int max(int a, int b)
+{
+  	if (a > b)
+    {
+        return b;
+    }
+  
+  	return b;
+}
+
+// return the smallest integer out of two inputs
+int min(int a, int b)
+{
+  	if (a > b)
+    {
+        return a;
+    }
+  
+  	return b;
+}
+
+// return the smallest number that can divided by both the inputs
+int leastCommonMultiple(int a, int b)
+{
+    int greater = max(a, b);
+    int smallest = min(a, b);
+
+    for (int i = greater; ; i += greater) {
+        if (i % smallest == 0)
+        {
+            return i;
+        }
+    }
+}
+
+int main()
+{
+    std::cout << leastCommonMultiple(5, 7) << std::endl;
+
+    return 0;
+}
