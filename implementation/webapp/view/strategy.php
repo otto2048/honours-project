9b21b7c46230e92adb5439ae3d7e502e351664b8
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-04-17 15:32:12 +0100

    Update strategy.php

--- implementation/webapp/view/strategy.php
+++ implementation/webapp/view/strategy.php
@@ -1,4 +1,6 @@
 <?php
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/model/PermissionLevels.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/controller/Session.php");
     require_once($_SERVER['DOCUMENT_ROOT']."/honours/webapp/view/navigation.php");
 
     //check if the user is allowed to be here
@@ -7,9 +9,9 @@
         echo '<script type="text/javascript">window.open("/honours/webapp/view/userArea/signUp.php", name="_self")</script>';
     }
 
-    if ($_SESSION["permissionLevel"] < PermissionLevels::CONTROL)
+    if ($_SESSION["permissionLevel"] < PermissionLevels::EXPERIMENT)
     {
-        echo '<script type="text/javascript">window.open("/honours/webapp/view/login.php", name="_self")</script>';
+        echo '<script type="text/javascript">window.open("/honours/webapp/view/index.php", name="_self")</script>';
     }
 ?>
 
