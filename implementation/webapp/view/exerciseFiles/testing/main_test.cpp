38046b16415947549dddf7c44d85a11dce5c0454
otto2048 <91140129+otto2048@users.noreply.github.com>
2023-04-21 15:42:47 +0100

    Output test vector at submission

--- implementation/webapp/view/exerciseFiles/testing/main_test.cpp
+++ implementation/webapp/view/exerciseFiles/testing/main_test.cpp
@@ -1,10 +1,37 @@
 #include "gtest/gtest.h"
+#include <string>
+
+using std::string;
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
   const int rv = RUN_ALL_TESTS();
 
+  string testVector = "";
+
+  //get test vector
+  for (int i = 0; i < ::testing::UnitTest::GetInstance()->total_test_suite_count(); i++)
+  {
+    const ::testing::TestSuite* testSuite = ::testing::UnitTest::GetInstance()->GetTestSuite(i);
+
+    for (int j = 0; j < testSuite->total_test_count(); j++)
+    {
+      const ::testing::TestInfo* testInfo = testSuite->GetTestInfo(j);
+
+      if (testInfo->result()->Passed())
+      {
+        testVector = testVector + "1";
+      }
+      else
+      {
+        testVector = testVector + "0";
+      }
+    }
+  }
+
+  std::cout << "TEST_VECTOR:" << std::endl;
+  std::cout << testVector << std::endl;
   std::cout << "DEBUGGING_TOOL_RESULT:" << std::endl;
   std::cout << ::testing::UnitTest::GetInstance()->successful_test_count() << std::endl;
 
